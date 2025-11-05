87893102c108b76cf8a06e793148b16140af9e76
coffin299 <coffin299@ymail.ne.jp>
2025-11-06 00:12:15 +0900

    refactor

--- MOMOKA/generator/imagen/__init__.py
+++
@@ -1,9 +0,0 @@
-from .model_registry import ImageModelInfo, ImageModelRegistry
-from .pipeline import GenerationParams, LocalTxt2ImgPipeline
-
-__all__ = [
-    "ImageModelInfo",
-    "ImageModelRegistry",
-    "GenerationParams",
-    "LocalTxt2ImgPipeline",
-]
